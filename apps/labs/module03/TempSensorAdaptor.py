d71e3ef456cd4790b5b54f9301f510ccf03619ea
aman shah <shah.ama@husky.neu.edu>
2020-02-15 22:28:32 -0500

    commit for  assignn w/o tests

--- apps/labs/module03/TempSensorAdaptor.py
+++ apps/labs/module03/TempSensorAdaptor.py
@@ -3,20 +3,27 @@ Created on 08-Feb-2020
 
 @author: deepak
 '''
+from labs.module03 import TempSensorAdaptorTask
+import sys
+'''
+Created on 08-Feb-2020
+
+@author: deepak
+'''
+from labs.module03 import TempSensorAdaptorTask
+import sys
 '''
 this python script starts the entire Temperature Hnadling process here. This is the main/start process
 '''
 
-from labs.module03 import TempSensorAdaptorTask
-import sys
-sys.path.insert(0,'/home/pi/workspace/iot-device/connected-devices-python/apps')
+sys.path.append('/home/pi/workspace/iot-device/apps')
 # print(sys.path)
 
 
 # instance of temperature Emulator created with threshold value
-alert_diff = 0.5
+alert_diff = 0.05
 simulator = TempSensorAdaptorTask.TempSensorAdaptorTask(alert_diff)
-# enabled the emulator
-simulator.setEmulator(True)
 #thread starting
 simulator.start()
+# enabled the emulator
+simulator.setEmulator(True)
